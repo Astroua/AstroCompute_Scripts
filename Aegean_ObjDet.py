1de509949c8576fe1fbc61676b6d5b249a1aee03
Alex Tetarenko <tetarenk@ualberta.ca>
2015-10-29 09:47:59 -0600

    object detection script

--- Aegean_ObjDet.py
+++ Aegean_ObjDet.py
@@ -19,10 +19,27 @@ import imp
 path_dir='/home/ubuntu/'
 
 def getVar(filename):
+    '''Easy way to read in parameters from file
+    '''
     f=open(filename)
     global data_params
     data_params=imp.load_source('data_params','',f)
     f.close()
+
+def run_aegean(tables,cellSize):
+    '''Open Aegean object file and read in source parameters
+    '''
+    with open(tables) as f:
+      lines=f.readlines()
+    for i in range(1,len(lines)):
+      lin_split=lines[i].split('\t')
+      src_list.append(lin_split[0])
+      ra_list.append(lin_split[4])#string
+      dec_list.append(lin_split[5])#string
+      maj_list.append(float(lin_split[14])/cellSize)#pix
+      min_list.append(float(lin_split[16])/cellSize)#pix
+      pos_list.append(float(lin_split[18]))#deg
+    return(src_list,ra_list,dec_list,maj_list,min_list,pos_list)
 
 ###########################################################
 #USER INPUT SECTION--> read in from parameters file
@@ -42,10 +59,10 @@ intervalSizeS = data_params.intervalSizeS
 # Label for casa output directories and files.
 label = target + '_' + refFrequency + '_' + obsDate + '_'
 outputPath = path_dir+'data_products/images_'+target+'_'+refFrequency+'_'+str(intervalSizeH)+'hours'+str(intervalSizeM)+'min'+str(intervalSizeS)+'sec/'
-
 fits_file=outputPath+label+'whole_dataset.fits'
 out_file0=outputPath+label+'whole_dataset_aegean.txt'
 tab_file=outputPath+label+'whole_dataset_objdet.tab'
+#aegean parameters
 seed=data_params.seed
 flood=data_params.flood
 tele=data_params.tele
@@ -63,14 +80,16 @@ min_list=[]
 pos_list=[]
 src_list=[]
 sources=[]
+#get latitude for telescope
 lat=aegean.scope2lat(tele)
+#adding SMA and NOEMA to list
 if lat==None:
     if tele=='SMA':
         lat=19.8243
     elif tele=='NOEMA':
         lat=44.6339
     else:
-        lat_string=raw_input('Enter latitude of telescope-->')
+        #lat_string=raw_input('Enter latitude of telescope-->')
         lat=float(lat_string))
 out_file= open(out_file0, 'w')
 tables=tab_file
@@ -88,5 +107,14 @@ out_file.close()
 if len(detections) == 0:
     print 'No sources detected'
 sources.extend(detections)
+#write detected source info to file
 if len(sources) > 0:
     save_catalog(tables, sources)
+
+#print detected sources fro user to choose from    
+src_l,ra_l,dec_l,maj_l,min_l,pos_l=run_aegean(tab_file,cellSize)
+print 'Number of Objects Detected is ', len(src_l)
+print 'Objects Detected-->'
+print 'Object, RA, DEC'
+for i in range(0,len(src_l)):
+    print src_l[i],ra_l[i],dec_l[i]

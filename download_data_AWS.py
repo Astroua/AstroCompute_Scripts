7c4b3ff9128bec2fafc189d9daa1c785fc7b5834
Alex Tetarenko <tetarenk@ualberta.ca>
2015-11-24 14:41:35 -0700

    removed my secret keys

--- download_data_AWS.py
+++ download_data_AWS.py
@@ -13,5 +13,5 @@ bucket_name='astrocompute_testbucket0'
 output_dir='datadownload'
 
 download_from_s3(key_name, bucket_name, conn=None,
-                     aws_access={'aws_access_key_id': 'AKIAJCAOONJFMHT2IIRA',
- 'aws_secret_access_key': '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'}, output_dir=output_dir)
+                     aws_access={'aws_access_key_id': 'ACCESS_KEY_HERE',
+ 'aws_secret_access_key': 'SECRET_ACCESS_KEY_HERE'}, output_dir=output_dir)

d8e04093da4e57d8f0a1179772f9d8d69173eb8d
Alex Tetarenko <tetarenk@ualberta.ca>
2015-11-24 15:45:24 -0700

    remove my specific values

--- remove_bucket_AWS.py
+++ remove_bucket_AWS.py
@@ -7,7 +7,7 @@ sys.path.append('PATH_TO_AWS_CONTROLLER')
 from upload_download_s3 import remove_s3_bucket
 from boto.s3.connection import S3Connection
 
-bucket_name='astrocompute_testbucket0'
+bucket_name='BUCKET_NAME'
 aws_access={'aws_access_key_id': 'ACCESS_KEY_HERE',
  'aws_secret_access_key': 'SECRET_ACCESS_KEY_HERE'}
 conn=conn = S3Connection(**aws_access)

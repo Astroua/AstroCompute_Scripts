afdef12d295891cd518c2c434f60043ded6f86e2
Alex Tetarenko <tetarenk@ualberta.ca>
2015-11-24 14:47:23 -0700

    removed secret key

--- remove_bucket_AWS.py
+++ remove_bucket_AWS.py
@@ -8,8 +8,8 @@ from upload_download_s3 import remove_s3_bucket
 from boto.s3.connection import S3Connection
 
 bucket_name='astrocompute_testbucket0'
-aws_access={'aws_access_key_id': 'AKIAJCAOONJFMHT2IIRA',
- 'aws_secret_access_key': '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'}
+aws_access={'aws_access_key_id': 'ACCESS_KEY_HERE',
+ 'aws_secret_access_key': 'SECRET_ACCESS_KEY_HERE'}
 conn=conn = S3Connection(**aws_access)
 
 remove_s3_bucket(bucket_name,conn)

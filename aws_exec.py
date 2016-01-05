cafdf24944c72ca0c24a8080aedd4693bcc8206d
e-koch <koch.eric.w@gmail.com>
2016-01-05 11:25:56 -0700

    Update control script

--- aws_exec.py
+++ aws_exec.py
@@ -1,15 +1,16 @@
 
 import json
-from time import sleep
+from time import sleep, time
 
 import boto
+from boto import sqs
 
 from aws_controller.controller import WORKER_SCRIPT
 from aws_controller.utils import timestring
 from aws_controller.launch_instance import launch
 
 
-def run(timestamp, param_file, aws_file, s3_bucket=None, log_file=None):
+def run(timestamp, param_file, aws_file):
     '''
     Run Timing scripts on AWS. See README for more info.
 
@@ -23,19 +24,25 @@ def run(timestamp, param_file, aws_file, s3_bucket=None, log_file=None):
     ----------
     timestamp : str
         Timestamp created when the request was submitted.
-    param_file : str
-        JSON file with parameter settings.
-    aws_file : str
-        JSON file with settings for the AWS execution.
+    param_file : str or dict
+        JSON file or a dictionary with parameter settings.
+    aws_file : str or dict
+        JSON file or a dictionary with settings for the AWS execution.
     '''
 
     # Read the param_file file
-    with open(param_file) as f:
-        params = json.load(f)
+    if isinstance(param_file, dict):
+        params = param_file
+    else:
+        with open(param_file) as f:
+            params = json.load(f)
 
     # Read the AWS settings file.
-    with open(aws_file) as f:
-        aws_settings = json.load(f)
+    if isinstance(aws_file, dict):
+        aws_settings = aws_file
+    else:
+        with open(aws_file) as f:
+            aws_settings = json.load(f)
 
     proc_name = params['target'] + "_" + timestamp
     key = aws_settings['key']
@@ -44,11 +51,11 @@ def run(timestamp, param_file, aws_file, s3_bucket=None, log_file=None):
 
     # Create the queue and submit the message.
     queue_name = proc_name
-    resp_name = "resp_" + proc_name
+    resp_queue_name = "resp_" + proc_name
 
-    queue = boto.sqs.connect_to_region(region,
-                                       aws_access_key_id=key,
-                                       aws_secret_access_key=secret).create_queue(queue_name)
+    queue = sqs.connect_to_region(region,
+                                  aws_access_key_id=key,
+                                  aws_secret_access_key=secret).create_queue(queue_name)
 
     msg = json_message(params, aws_settings, proc_name)
     queue.write(queue.new_message(body=msg))
@@ -61,28 +68,59 @@ def run(timestamp, param_file, aws_file, s3_bucket=None, log_file=None):
          "KEY": key,
          "SECRET": secret,
          "REGION": region,
-         "RESP_QUEUE_NAME": resp_name}
+         "RESP_QUEUE_NAME": resp_queue_name}
 
     inst = launch(key, region=region, image_id=aws_settings['image_id'],
                   instance_type=aws_settings['instance_type'],
                   security_groups=aws_settings['security_groups'],
                   initial_check=False, user_data=user_data)
 
+    # Connect to the response queue
+    resp_queue = sqs.connect_to_region(region,
+                                       aws_access_key_id=key,
+                                       aws_secret_access_key=secret).create_queue(resp_queue_name)
+
     # Wait for return message
-    while inst.state == "running":
-        sleep(60)
+    # Total wait time set to 1 day
+    time_max = 3600 * 24
+    time_wait = 600
+    t0 = time()
+    while time() < t0 + time_max:
+        update = inst.update()
+        if update in [u"stopping", u"stopped"]:
+            break
+        # If a message is received, can be assumed to be done
+        elif resp_queue.count() > 0:
+            break
+        sleep(time_wait)
+    else:
+        print("Reached time limit. Terminating.")
+
+    inst.terminate()
+
+    # Check the response message
+    mess = resp_queue.read(10)
+    if mess is not None:
+        content = json.loads(mess.get_body())
+        print("Saving content.")
+        with open("tests/test_response.txt", "w") as f:
+            json.dump(content, f)
+
+    # Clean-up queues
+    queue.delete()
+    resp_queue.delete()
 
     # Return success/failure
 
 
-def json_message(params, aws_settings, proc_name, s3_bucket=None):
+def json_message(params, aws_settings, proc_name):
     '''
     Create the json message to feed to the worker instance.
     '''
 
     # Command to run
     cmd = ["/usr/local/bin/CASA/casa-release-4.3.1-el6/casa", "--nologger",
-           "--logfile", proc_name+".log",
+           "--logfile", proc_name+".log", "-c",
            "/home/ubuntu/AstroCompute_Scripts/casa_timing_script.py",
            "/home/ubuntu/data/params.txt", "/home/ubuntu/"]
 
@@ -90,12 +128,7 @@ def json_message(params, aws_settings, proc_name, s3_bucket=None):
 
     mess['proc_name'] = proc_name
     mess['key_name'] = "data/*"
-
-    if s3_bucket is not None:
-        mess['bucket'] = s3_bucket
-    else:
-        mess['bucket'] = proc_name
-
+    mess['bucket'] = proc_name
     mess["command"] = cmd
 
     return json.dumps(mess)

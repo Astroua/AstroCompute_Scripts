4810e2f812b48a777f6eac6dd7da5a6b5c73db4d
e-koch <koch.eric.w@gmail.com>
2016-02-01 16:06:25 -0700

    Just call it application

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
 
 
 from flask import Flask, request, redirect, url_for, \
-    render_template, send_from_directory, jsonify
+    render_template, send_from_directory, jsonify, request
 from astropy import log
 from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
+
+from aws_controller.upload_download_s3 import upload_to_s3
+
 
 UPLOAD_FOLDER = 'uploads/'
 
@@ -15,7 +18,6 @@ app.secret_key = 'cheese'
 
 key = app.config['S3_KEY']
 secret = app.config['S3_SECRET']
-bucket = app.config['S3_BUCKET']
 
 
 class MyForm(Form):
@@ -35,6 +37,18 @@ def submit():
         return redirect('/success')
     return render_template('submit.html', form=form)
 
+
+@app.route('/upload', methods=['POST'])
+def upload(timestamp):
+    if request.method == 'POST':
+        data_file = request.files.get('file')
+        file_name = data_file.filename
+        upload_to_s3(timestamp, file_name, create_bucket=True,
+                     aws_access={"aws_access_key_id": key,
+                                 "aws_secret_access_key": secret})
+
+        # return jsonify(name=file_name)
+        return jsonify(name=file_name)
 
 if __name__ == '__main__':
     log.setLevel(10)

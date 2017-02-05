00c97cb8c18e0f3e8025f036257f33e336b5bcc2
Alexandra Tetarenko <atetarenk@Alexandra-Tetarenkos-MacBook-Pro.local>
2017-02-04 17:31:29 -0700

    turned into module to be used in timing script

--- Aegean_ObjDet.py
+++ Aegean_ObjDet.py
@@ -1,103 +1,36 @@
-'''Object Detection script-->Using AEGEAN algorithm (https://github.com/PaulHancock/Aegean)
-Input: Cleaned FITS image of whole data set
-Output: Data file of the parameters of objects found in image
-NOTE: Run in normal python, DO NOT RUN WITHIN CASA, it won't work.
-Written by A. Tetarenko'''
+##################################
+#Object Detection Script
+##################################
+'''Uses AEGEAN algorithm (https://github.com/PaulHancock/Aegean) for object detection in a radio image.
+INPUT: Cleaned FITS image of whole data set--> [fits_file]
+OUTPUT: Data file of the parameters of objects found in image--> [tab_file]
+NOTE: Needs to be run with lmfit 0.7.4, otherwise it won't work.
+(casa-pip install git+git://github.com/lmfit/lmfit-py.git@0.7.4)
+
+Written by: A. Tetarenko
+Last Updated: February 2 2017'''
 
 # Import modules
 # Make sure aegean tree of directories in path so you can import it
 import sys
 sys.path.append('/home/ubuntu/Aegean')
 import aegean
-
 from AegeanTools.catalogs import save_catalog
 import numpy as np
 from multiprocessing import cpu_count
 import re
 import os
+from utils import run_aegean,initial_clean
+import warnings
+warnings.filterwarnings('ignore')
 
+def objdet(tele,lat,out_file0,fits_file,seed,flood,tab_file,catalog_input_name,cellSize_string):
+  sources = []
 
-# set path to where output is to be stored-->need to set up file system so have data and data_products directory
-# in this path
-path_dir = sys.argv[-1]
-# path_dir='/home/ubuntu/'
-
-param_file = sys.argv[-2]
-
-
-def run_aegean(tables, cellSize):
-    '''Open Aegean object file and read in source parameters
-    '''
-
-    ra_list = []
-    dec_list = []
-    maj_list = []
-    min_list = []
-    pos_list = []
-    src_list = []
-
-    with open(tables) as f:
-        lines = f.readlines()
-    for i in range(1, len(lines)):
-        lin_split = lines[i].split('\t')
-        src_list.append(lin_split[0])
-        ra_list.append(lin_split[4])  # string
-        dec_list.append(lin_split[5])  # string
-        maj_list.append(float(lin_split[14]) / cellSize)  # pix
-        min_list.append(float(lin_split[16]) / cellSize)  # pix
-        pos_list.append(float(lin_split[18]))  # deg
-    return(src_list, ra_list, dec_list, maj_list, min_list, pos_list)
-
-###########################################################
-# USER INPUT SECTION--> read in from parameters file
-###########################################################
-sys.path.append(os.path.join(path_dir, "AstroCompute_Scripts/"))
-from utils import load_json,convert_param_format
-
-data_params = load_json(param_file)
-#to convert txt param file to dictionary do this,
-#data_params = convert_param_format(param_file, to="dict")
-
-# Target name.
-target = data_params["target"]
-# Date of observation.
-obsDate = data_params["obsDate"]
-# Observation frequency.
-refFrequency = data_params["refFrequency"]
-# Length of time bins (H,M,S)
-intervalSizeH = data_params["intervalSizeH"]
-intervalSizeM = data_params["intervalSizeM"]
-intervalSizeS = data_params["intervalSizeS"]
-# Label for casa output directories and files.
-label = target + '_' + refFrequency + '_' + obsDate + '_'
-outputPath = \
-    os.path.join(path_dir, 'data_products/images_' + target + '_' + refFrequency +
-                 '_' + str(intervalSizeH) + 'hours' + str(intervalSizeM) + 'min' +
-                 str(intervalSizeS) + 'sec/')
-fits_file = os.path.join(outputPath, label + 'whole_dataset.fits')
-out_file0 = os.path.join(outputPath, label + 'whole_dataset_aegean.txt')
-tab_file = os.path.join(outputPath, label + 'whole_dataset_objdet_comp.tab')
-catalog_input_name = os.path.join(outputPath, label + 'whole_dataset_objdet.tab')
-# aegean parameters
-seed = data_params["seed"]
-flood = data_params["flood"]
-tele = data_params["tele"]
-cellSize_string = data_params["cellSize"]
-cellSize_list = re.findall('\d+|\D+', cellSize_string)
-cellSize = float(cellSize_list[0] + cellSize_list[1] + cellSize_list[2])
-
-###########################################################
-# END OF USER INPUT SECTION
-###########################################################
-
-
-# use Aegean object detection algorithm--> import as module
-sources = []
-
-# get latitude for telescope
-lat = aegean.scope2lat(tele)
-# adding SMA and NOEMA to list
-if lat == None:
+  # get latitude for telescope
+  lat = aegean.scope2lat(tele)
+  # adding SMA and NOEMA to list
+  if lat == None:
     if tele == 'SMA':
         lat = 19.8243
     elif tele == 'NOEMA':
@@ -106,10 +39,10 @@ if lat == None:
         #lat_string=raw_input('Enter latitude of telescope-->')
         lat = data_params["lat"]
 
-out_file = open(out_file0, 'w')
+  out_file = open(out_file0, 'w')
 
-print 'Running Aegean Object Detection -->'
-detections = aegean.find_sources_in_image(fits_file,
+  print 'Running Aegean Object Detection -->'
+  detections = aegean.find_sources_in_image(fits_file,
                                           outfile=out_file,
                                           hdu_index=0,
                                           rms=None,
@@ -123,19 +56,50 @@ detections = aegean.find_sources_in_image(fits_file,
                                           nonegative=not False,
                                           nopositive=False,
                                           mask=None, lat=lat, imgpsf=None)
-out_file.flush()
-out_file.close()
-if len(detections) == 0:
+  out_file.flush()
+  out_file.close()
+  if len(detections) == 0:
     raise Exception('No sources detected by Aegean. Please check your inputs.')
-sources.extend(detections)
-# write detected source info to file
-if len(sources) > 0:
+  sources.extend(detections)
+  # write detected source info to file
+  if len(sources) > 0:
     save_catalog(catalog_input_name, sources)
 
-# print detected sources fro user to choose from
-src_l, ra_l, dec_l, maj_l, min_l, pos_l = run_aegean(tab_file, cellSize)
-print 'Number of Objects Detected is ', len(src_l)
-print 'Objects Detected-->'
-print 'Object, RA, DEC'
-for i in range(0, len(src_l)):
+  # print detected sources fro user to choose from
+  src_l, ra_l, dec_l, maj_l, min_l, pos_l = run_aegean(tab_file, cellSize_string)
+  print 'Number of Objects Detected is ', len(src_l)
+  print 'Objects Detected-->'
+  print 'Object, RA, DEC'
+  for i in range(0, len(src_l)):
     print src_l[i], ra_l[i], dec_l[i]
+  return(src_l, ra_l, dec_l, maj_l, min_l, pos_l)
+
+if __name__ == "__main__":
+  ##################################
+  #User Input Section and Setup
+  ##################################
+  # set paths
+  path_dir = '/mnt/bigdata/tetarenk/timing_test/'#make sure to including trailing / !!!
+  path_dir_ac='/home/ubuntu/'
+
+  sys.path.append(os.path.join(path_dir_ac, "AstroCompute_Scripts/"))
+  ##################################
+
+  ##################################
+  #Reading in Parameters
+  ##################################
+  # Label for casa output directories and files.
+  fits_file = path_dir+'data_products/images_Test_Date_6GHz_0hours0min10sec/Test_6GHz_Date_whole_dataset.fits'
+  out_file0 = path_dir+'data_products/images_Test_Date_6GHz_0hours0min10sec/Test_6GHz_Date_whole_dataset_aegean.txt'
+  tab_file = path_dir+'data_products/images_Test_Date_6GHz_0hours0min10sec/Test_6GHz_Date_whole_dataset_objdet_comp.tab'
+  catalog_input_name = path_dir+'data_products/images_Test_Date_6GHz_0hours0min10sec/Test_6GHz_Date_whole_dataset_objdet.tab'
+  # aegean parameters
+  seed = 10
+  flood = 4
+  tele = 'VLA'
+  cellSize_string = '0.3arcsec'
+  ##################################
+
+  #run aegean
+  src_l, ra_l, dec_l, maj_l, min_l, pos_l=objdet(tele,lat,out_file0,fits_file,seed,flood,tab_file,catalog_input_name,cellSize_string)
+
